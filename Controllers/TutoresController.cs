8780d4b63c36ad9163d4ca583543915366e6e9fc
William <85803551+Kevin-Rodriguez49@users.noreply.github.com>
2021-06-12 20:18:58 -0600

    Add files via upload

---
+++ Controllers/TutoresController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Parcial_Tutores.Models;
+
+namespace Parcial_Tutores.Controllers
+{
+    public class TutoresController : Controller
+    {
+        // GET: Tutores
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Tutores()
+        {
+            Tutoria objTutor = new Tutoria();
+            objTutor.Nombre = "Erick";
+            objTutor.Apellido = "Garcia";
+            objTutor.NombreCatedratico = "Jorge Machado";
+            objTutor.NombreFacultad = "Ciencias Empresariales";
+           // objTutor.NombreMateria = "Programacion II";
+            objTutor.DiaClase = "Sabados";
+            objTutor.HorarioClase = "8:00";
+            return View(objTutor);
+        }
+        public ActionResult Tutor()
+        {
+            Tutoria objTutor = new Tutoria();
+            objTutor.Nombre = Request.Form["Nombre del Tutor"].ToString();
+            return View();
+        }
+    }
+}
---
+++ Models/Tutoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial_Tutores.Models
+{
+    public class Tutoria
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreCatedratico { get; set; }
+        public string NombreFacultad { get; set; }
+        public string NombreCarrera { get; set; }
+        public string NombreMateria { get; set; }
+        public string DiaClase { get; set; }
+        public string HorarioClase { get; set; }
+    }
+}
